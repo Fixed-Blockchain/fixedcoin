14cdbfee9e3924de0ff3112804192ed80a684bf6
Senasgr <senasgr-eth@example.com>
2025-12-03 21:48:05 +0700

    fix small detail

--- src/kernel/chainparams.cpp
+++ src/kernel/chainparams.cpp
@@ -116,7 +116,7 @@ public:
         consensus.vDeployments[Consensus::DEPLOYMENT_TAPROOT].min_activation_height = 0; 
 
         consensus.nMinimumChainWork = uint256{"000000000000000000000000000000000000000000000c590abaf503466e7293"};
-        consensus.defaultAssumeValid = uint256{"00000000000000000018c2807d70584ee59b802566be062f21eed3737ec99f13d"}; // 931
+        consensus.defaultAssumeValid = uint256{"0000000000000000018c2807d70584ee59b802566be062f21eed3737ec99f13d"}; // 931
 
         /**
          * The message start string is designed to be unlikely to occur in normal data.
